fd92154df01b2391fc1703e93486af5999939031
Vedant0898 <97685507+Vedant0898@users.noreply.github.com>
2022-03-14 19:29:44 +0530

    Completed captcha task

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,25 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector(".loginpanel"); // Select login element
 let login_text = login_element.innerText;
+let arr = login_text.split(/\n/);
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let question = arr[4]; // Get question from login_element
+let question_arr = question.split(' ');
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+let answer;
+if (question.includes('add')){
+    val_1 = parseInt(question_arr[2]);
+    val_2 = parseInt(question_arr[4]);
+    answer = val_1+val_2
+} else if (question.includes('subtract')) {
+    val_1 = parseInt(question_arr[2]);
+    val_2 = parseInt(question_arr[4]);
+    answer = val_1-val_2
+} else if (question.includes('first')) {
+    answer = question_arr[4];
+} else {
+    answer = question_arr[6];
+}
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+const captcha_input_element = document.querySelector("#valuepkg3"); // Select input element
 captcha_input_element.value = answer;
